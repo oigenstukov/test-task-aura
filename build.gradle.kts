2321750f2aa5771ed835fe97b094533be4dd1e99
Evgeny Podskrebkin <oigenstukov@gmail.com>
2025-09-08 00:41:57 +0200

    fix: core files

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.21.0")
+    testImplementation("io.rest-assured:rest-assured:5.5.2")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    testImplementation("org.slf4j:slf4j-simple:2.0.12")
+    compileOnly("org.projectlombok:lombok:1.18.40")
+    annotationProcessor("org.projectlombok:lombok:1.18.40")
+    testCompileOnly("org.projectlombok:lombok:1.18.40")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.40")
 }
 
 tasks.test {
